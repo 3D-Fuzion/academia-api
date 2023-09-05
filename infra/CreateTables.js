3f0de7dafe982247479a10fda5496316e1492daf
Guilherme-Vianna <mataveli91@gmail.com>
2023-09-05 15:47:09 -0300

    feat: Migracoes alteradas

--- infra/CreateTables.js
+++ infra/CreateTables.js
@@ -1,28 +1,39 @@
 const mysql = require("mysql2/promise");
+require("dotenv").config();
+
 async function CreateTables() {
   let connection = mysql.createPool({
-    host: "localhost",
-    user: "academia-api",
-    password: "6414",
-    port: "3306",
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
   });
   console.log("Creating database...");
-  await connection.query("DROP DATABASE IF EXISTS `migration-test`");
-  await connection.query("CREATE DATABASE `migration-test`");
+  console.log(process.env.DATABASE_NAME);
+  let response;
+  response = await connection.query(
+    "DROP DATABASE IF EXISTS `" + process.env.DATABASE_NAME + "`"
+  );
+  console.log(response);
+
+  response = await connection.query(
+    "CREATE DATABASE `" + process.env.DATABASE_NAME + "`"
+  );
+  console.log(response);
 
   connection = mysql.createPool({
-    host: "localhost",
-    user: "academia-api",
-    password: "6414",
-    database: "migration-test",
-    port: "3306",
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
   });
 
   console.log("User tables creation started...");
-  const [result] = await connection.query(
+  response = await connection.query(
     "CREATE TABLE `user` (id int AUTO_INCREMENT PRIMARY KEY, name varchar(64) NOT NULL, email varchar(64) NOT NULL, cpf char(11) NOT NULL, password varchar(128) NOT NULL, birthdate date NOT NULL)"
   );
-  console.log(result);
+  console.log(response);
 }
 
 CreateTables();
