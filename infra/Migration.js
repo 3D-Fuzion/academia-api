e593825880d470afe9dde23a9252f06811a5ff57
Guilherme Vianna <mataveli91@gmail.com>
2023-09-05 19:20:05 -0300

    feat: Arquivo de migracao atualizado.

---
+++ infra/Migration.js
@@ -0,0 +1,49 @@
+const mysql = require("mysql2/promise");
+require("dotenv").config();
+
+async function CreateDB() {
+  const connection = mysql.createPool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    connectionLimit: 1,
+  });
+  console.log("Creating database...");
+  let response;
+  response = await connection.query(
+    "DROP DATABASE IF EXISTS `" + process.env.DATABASE_NAME + "`"
+  );
+  console.log(response);
+  response = await connection.query(
+    "CREATE DATABASE `" + process.env.DATABASE_NAME + "`"
+  );
+  console.log(response);
+  connection.end();
+}
+
+async function CreateTables() {
+  const connection = mysql.createPool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+  });
+
+  console.log("Inserting Tables...");
+  response = await connection.query(
+    "CREATE TABLE `user` (id int AUTO_INCREMENT PRIMARY KEY, name varchar(64) NOT NULL, email varchar(64) NOT NULL, cpf char(11) NOT NULL, password varchar(128) NOT NULL, birthdate date NOT NULL, academyCode int(8) NOT NULL,registerStatus enum('waiting', 'accepted') NOT NULL)"
+  );
+
+  console.log(response);
+  connection.end();
+}
+
+async function CreateDataBase() {
+  await CreateDB();
+  await CreateTables();
+  console.log("Migration Run Sucessfuly");
+}
+
+CreateDataBase();
