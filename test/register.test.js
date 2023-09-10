a943cd1952f97e74de9bece0c3c006912711706c
Guilherme Vianna <mataveli91@gmail.com>
2023-09-10 19:34:34 +0000

    test: Teste com sintaxe correta adicionado

--- test/register.test.js
+++ test/register.test.js
@@ -1,10 +1,20 @@
-const request = require("supertest"); 
+const request = require("supertest");
 
-describe("Testando Cadastro", () => { 
-  it("Sei la ", async () => { 
-    const res = await request('http://localhost:3333').get("/manager/solicitation")
+describe("Cadastro de Usuarios", () => {
+  it("Sintaxe correta", async () => {
+    const res = await request('http://localhost:3333')
+      .post("/user")
+      .send
+      ({
+        name: "TESTCadastro",
+        email: "exemploteste@email.com",
+        password: "123123123",
+        cpf: "15918390769",
+        academycode: "88888888",
+        birthdate: "2003-07-23"
+      });
     expect(res.statusCode).toEqual(200)
   })
 });
 
- 
+
