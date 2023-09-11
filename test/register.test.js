54e762e3bba2326ed29784aed72dd6055e0c39f7
Guilherme-Vianna <mataveli91@gmail.com>
2023-09-11 10:45:06 -0300

    test: teste de registro adicionado e migracao configurada para rodar antes dos testes

--- test/register.test.js
+++ test/register.test.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 
 describe("Cadastro de Usuarios", () => {
-  it("Sintaxe correta", async () => {
+  it("Sintaxe correta | CADASTRO PADRAO |", async () => {
     const res = await request('http://localhost:3333')
       .post("/user")
       .send
@@ -13,7 +13,93 @@ describe("Cadastro de Usuarios", () => {
         academycode: "88888888",
         birthdate: "2003-07-23"
       });
-    expect(res.statusCode).toEqual(200)
+    expect(res.statusCode).toEqual(201)
+  })
+ it("Sintaxe correta | CADASTRO DUPLICADO |", async () => {
+    const res = await request('http://localhost:3333')
+      .post("/user")
+      .send
+      ({
+        name: "TESTCadastro",
+        email: "exemploteste@email.com",
+        password: "123123123",
+        cpf: "15918390769",
+        academycode: "88888888",
+        birthdate: "2003-07-23"
+      });
+    expect(res.statusCode).toEqual(400)
+  })
+
+it("Sintaxe incorreta | SENHA PEQUENA |", async () => {
+    const res = await request('http://localhost:3333')
+      .post("/user")
+      .send
+      ({
+        name: "TESTCadastro",
+        email: "exemploteste@email.com",
+        password: "123123",
+        cpf: "15918390769",
+        academycode: "88888888",
+        birthdate: "2003-07-23"
+      });
+    expect(res.statusCode).toEqual(400)
+  }) 
+it("Sintaxe incorreta | CPF FALTANDO NUMERO |", async () => {
+    const res = await request('http://localhost:3333')
+      .post("/user")
+      .send
+      ({
+        name: "TESTCadastro",
+        email: "exemploteste@email.com",
+        password: "123123",
+        cpf: "1591839076",
+        academycode: "88888888",
+        birthdate: "2003-07-23"
+      });
+    expect(res.statusCode).toEqual(400)
+  })
+it("Sintaxe incorreta | CPF CONTENDO LETRA |", async () => {
+    const res = await request('http://localhost:3333')
+      .post("/user")
+      .send
+      ({
+        name: "TESTCadastro",
+        email: "exemploteste@email.com",
+        password: "123123",
+        cpf: "1591839a766",
+        academycode: "88888888",
+        birthdate: "2003-07-23"
+      });
+    expect(res.statusCode).toEqual(400)
+  })
+it("Sintaxe incorreta | ACADEMYCODE NAO TEM 8 NUMEROS |", async () => {
+    const res = await request('http://localhost:3333')
+      .post("/user")
+      .send
+      ({
+        name: "TESTCadastro",
+        email: "exemploteste@email.com",
+        password: "123123",
+        cpf: "1591839076",
+        academycode: "8888888",
+        birthdate: "2003-07-23"
+      });
+    expect(res.statusCode).toEqual(400)
+  })
+
+it("Sintaxe incorreta | ACADEMYCODE ACADEMIA NAO ENCONTRADA |", async () => {
+    const res = await request('http://localhost:3333')
+      .post("/user")
+      .send
+      ({
+        name: "TESTCadastro",
+        email: "exemploteste@email.com",
+        password: "123123",
+        cpf: "1591839076",
+        academycode: "77777777",
+        birthdate: "2003-07-23"
+      });
+    expect(res.statusCode).toEqual(400)
   })
 });
 
