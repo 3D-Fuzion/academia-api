10372b2c47833b3ffa911e9640aba32f4b881469
Guilherme-Vianna <mataveli91@gmail.com>
2023-09-12 15:38:31 -0300

    feat: Verificao de email alterada e verificacao de campos adicionada.

--- src/connection.js
+++ src/connection.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2/promise");
 
 const connection = mysql.createPool({
-  host: "localhost",
-  user: "academia-api",
-  password: "6414",
-  database: "academia-dev",
-  port: "3306",
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
+  database: process.env.DATABASE_NAME,
   connectionLimit: 2,
 });
 
