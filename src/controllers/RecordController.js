445fa1dc6639207f9b6ed9e05214fa1155dcbc53
Guilherme-Vianna <mataveli91@gmail.com>
2023-09-13 14:30:25 -0300

    feat: Alteracao de record adicionada

---
+++ src/controllers/RecordController.js
@@ -0,0 +1,83 @@
+const { json } = require("express");
+const mysql = require("mysql2/promise");
+
+async function setRecord(req, res) {
+  let connection = mysql.createPool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    database: process.env.DATABASE_NAME,
+    connectionLimit: 1,
+  });
+
+  const body = req.body;
+
+  const [rows] = await connection.query("SELECT id FROM `record` WHERE userId = ? AND trainingId = ? LIMIT 1",[
+    body.userid, body.trainingid
+  ]); 
+
+  if (rows.length > 0){
+    const recordId = rows[0].id 
+
+    const [changedExercice] = await connection.query("UPDATE `record` SET weight = ? WHERE id= ?", [
+      body.weight,
+      recordId,
+    ]);
+
+    if(changedExercice.affectedRows > 0){ 
+      res.status(200).end();
+    }
+  } 
+
+  if (rows.length < 1){
+    const [firstRecord] = await connection.query("INSERT INTO `record` (userId, trainingId, weight) VALUES (?,?,?)", [
+      body.userid, 
+      body.trainingid,
+      body.weight 
+    ]); 
+
+    if(firstRecord.affectedRows > 0){ 
+      res.status(200).end();
+    }
+  } 
+
+  connection.end();
+  res.status(200).end();
+}
+
+async function getTrainings (req, res) { 
+  let connection = mysql.createPool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    database: process.env.DATABASE_NAME,
+    connectionLimit: 1,
+  });
+
+  const category = req.query.category
+
+
+  if(!category) { 
+    const [trainings] = await connection.query("SELECT SQL_SMALL_RESULT * FROM `training`"); 
+    connection.end();
+    res.status(200).json(trainings).end();
+  } 
+
+  const [trainings] = await connection.query("SELECT SQL_SMALL_RESULT * FROM `training` WHERE category = ?", [
+    category
+  ]); 
+
+  if (trainings.length < 1) { 
+    res.status(404).json({message: "Not exist trainings with given category"}).end();  
+  }
+
+  connection.end();
+  res.status(200).json(trainings).end();
+}
+
+module.exports = {
+  setRecord,
+  getTrainings,
+};
--- src/router.js
+++ src/router.js
@@ -4,24 +4,49 @@ const loginController = require("./controllers/LoginController");
 const managerController = require("./controllers/ManagerController");
 const loginMiddleware = require("./controllers/middlewares/LoginMiddleware");
 const serverStatusController = require("./controllers/ServerStatusController");
+const recordController = require("./controllers/RecordController")
 
 router.post(
   "/user",
   loginMiddleware.validadeCreateUserBody,
   loginController.createUser
 );
+
 router.post(
   "/login",
   loginMiddleware.validadeCredentialsBody,
   loginController.generateToken
 );
+
 router.patch(
   "/login/changepwd",
   loginMiddleware.validadeChangePwdBody,
   loginController.changePassword
 );
-router.put("/manager/solicitation", managerController.acceptSolicitaction);
-router.get("/manager/solicitation", managerController.checkSolicitations);
-router.get("", serverStatusController.serverStatus);
+
+router.get(
+  "/training",
+  recordController.getTrainings
+); 
+
+router.put(
+  "/training",
+  recordController.setRecord
+); 
+
+router.get(
+  "", 
+  serverStatusController.serverStatus
+);
+
+router.get(
+  "/manager/solicitation", 
+  managerController.checkSolicitations
+);
+
+router.put(
+  "/manager/solicitation", 
+  managerController.acceptSolicitaction
+);
 
 module.exports = router;
