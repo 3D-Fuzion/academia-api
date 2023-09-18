d407f64abcb23bd91dbbb184438c17719cd56823
Guilherme Vianna <mataveli91@gmail.com>
2023-09-18 20:01:34 -0300

    feat: Campo ImagemURL, Sexo e PalavraDeEfeito adicionado

--- src/infra/Migration.js
+++ src/infra/Migration.js
@@ -35,31 +35,50 @@ async function CreateTables() {
 
   console.log("Inserting Tables...");
 
-  response = await connection.query(
-    "CREATE TABLE `user` (id int AUTO_INCREMENT PRIMARY KEY, name varchar(64) NOT NULL, email varchar(64) NOT NULL, cpf char(11) NOT NULL, password varchar(128) NOT NULL, birthdate date NOT NULL, academyCode int(8) NOT NULL,registerStatus enum('waiting', 'accepted') NOT NULL)"
+  response = await connection.query("CREATE TABLE `user` ("+
+    "id int AUTO_INCREMENT PRIMARY KEY,"+
+    "name varchar(64) NOT NULL,"+ 
+    "email varchar(64) NOT NULL,"+
+    "cpf char(11) NOT NULL,"+ 
+    "password varchar(128) NOT NULL,"+ 
+    "birthdate date NOT NULL,"+ 
+    "academyCode int(8) NOT NULL,"+
+    "registerStatus enum('waiting', 'accepted') NOT NULL,"+
+    "sex varchar(16),"+ 
+    "effectPhrase varchar(128),"+
+    "imageUrl varchar(255))"
   );
   console.log("Users table added");
 
-  response = await connection.query(
-    "CREATE TABLE `academy` (id int AUTO_INCREMENT PRIMARY KEY, name varchar(64) NOT NULL, code int NOT NULL)"
+  response = await connection.query("CREATE TABLE `academy` ("+
+    "id int AUTO_INCREMENT PRIMARY KEY,"+
+    "name varchar(64) NOT NULL, code int NOT NULL)"
   );
+
   console.log("Academy table added");
 
-  response = await connection.query(
-    "INSERT INTO `academy` (name, code) VALUES ('DefaultAcademy', '88888888')"
+  response = await connection.query("INSERT INTO `academy`"+ 
+    "(name, code)"+ 
+    "VALUES"+
+    "('DefaultAcademy', '88888888')"
   );
   console.log("Academy data insert");
 
-  response = await connection.query(
-    "CREATE TABLE `record` (id int AUTO_INCREMENT PRIMARY KEY, userId int NOT NULL, trainingId int NOT NULL, weight int NOT NULL)"
+  response = await connection.query("CREATE TABLE `record` ("+
+    "id int AUTO_INCREMENT PRIMARY KEY,"+
+    "userId int NOT NULL,"+
+    "trainingId int NOT NULL,"+
+    "weight int NOT NULL)"
   );
   console.log("Record table added");
 
-  response = await connection.query(
-    "CREATE TABLE `training` (id int AUTO_INCREMENT PRIMARY KEY, name varchar(64), category varchar(64))"
+  response = await connection.query("CREATE TABLE `training` ("+
+    "id int AUTO_INCREMENT PRIMARY KEY,"+
+    "name varchar(64),"+
+    "category varchar(64))"
   );
-  console.log("Training table added");
 
+  console.log("Training table added");
   connection.end();
 }
 
