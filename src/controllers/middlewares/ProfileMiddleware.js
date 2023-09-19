ca8dd2e2ccc85b52c6ae1c6bb9408a6881db9974
Guilherme-Vianna <mataveli91@gmail.com>
2023-09-19 15:00:11 -0300

    feat: Verificao de corpo e de existencia de usuario

--- src/controllers/middlewares/ProfileMiddleware.js
+++ src/controllers/middlewares/ProfileMiddleware.js
@@ -1,5 +1,15 @@
+const mysql = require("mysql2/promise");
+
 const validadeSetImageBody = async (req, res, next) => { 
-  
+  let connection = mysql.createPool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    database: process.env.DATABASE_NAME,
+    connectionLimit: 1,
+  });
+
   const body = req.body;
 
   if(!body.userid) { 
@@ -10,6 +20,17 @@ const validadeSetImageBody = async (req, res, next) => {
     return res.status(400).json({message: "EFFECT_PHRASE is required!"});
   }
 
+  const query = "SELECT * from `user` WHERE id = ? LIMIT 1"
+
+  const [rows] = await connection.execute(query,[ 
+    body.userid
+  ]);
+
+  if(rows.length < 1) { 
+    return res.status(404).json({message: "User not found"});
+  }
+
+  connection.end(); 
   next(); 
 }
 
