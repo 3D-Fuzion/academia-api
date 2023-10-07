dfa6cb860740552b5f52a3409e0a59186e88bb7a
Guilherme Vianna <59426923+Guilherme-Vianna@users.noreply.github.com>
2023-10-06 22:29:30 -0300

    fix format erro when edit birthdate

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -1,8 +1,8 @@
 const { json } = require("express");
 const jwt = require("jsonwebtoken");
 const mysql = require("mysql2/promise");
-const axios = require('axios'); 
-const FormData = require('form-data');
+const axios = require("axios");
+const FormData = require("form-data");
 
 require("dotenv").config();
 
@@ -16,23 +16,19 @@ const changeSex = async (request, response) => {
     connectionLimit: 1,
   });
 
-  const query =
-    "UPDATE `user` SET sex = ? WHERE id = ? LIMIT 1";
+  const query = "UPDATE `user` SET sex = ? WHERE id = ? LIMIT 1";
 
   const body = request.body;
 
-  const [rows] = await connection.execute(query, [
-    body.sex,
-    body.userid
-  ]);
-  
+  const [rows] = await connection.execute(query, [body.sex, body.userid]);
+
   connection.end();
 
-  if(rows.affectedRows > 0) { 
-  	return response.status(200).end();
+  if (rows.affectedRows > 0) {
+    return response.status(200).end();
   }
-  
-  return response.status(500).json({message:"Server Error!"});
+
+  return response.status(500).json({ message: "Server Error!" });
 };
 
 const changeEffectPhrase = async (request, response) => {
@@ -45,23 +41,22 @@ const changeEffectPhrase = async (request, response) => {
     connectionLimit: 1,
   });
 
-  const query =
-    "UPDATE `user` SET effectPhrase = ? WHERE id = ? LIMIT 1";
+  const query = "UPDATE `user` SET effectPhrase = ? WHERE id = ? LIMIT 1";
 
   const body = request.body;
 
   const [rows] = await connection.execute(query, [
     body.effectphrase,
-    body.userid
+    body.userid,
   ]);
 
   connection.end();
 
-  if(rows.affectedRows > 0) { 
-  	return response.status(200).end();
+  if (rows.affectedRows > 0) {
+    return response.status(200).end();
   }
 
-  return response.status(500).json({message: "Server Error!"});
+  return response.status(500).json({ message: "Server Error!" });
 };
 
 const setImage = async (request, response) => {
@@ -76,52 +71,47 @@ const setImage = async (request, response) => {
 
   const body = request.body;
 
-  if(!body.userid) { 
-  	response.status(400).json({message: "USERID is required!"});
+  if (!body.userid) {
+    response.status(400).json({ message: "USERID is required!" });
   }
 
-  if(!body.image) { 
-  	response.status(400).json({message: "IMAGE is required!"});
+  if (!body.image) {
+    response.status(400).json({ message: "IMAGE is required!" });
   }
 
   const form = new FormData();
-  form.append('image', body.image);
+  form.append("image", body.image);
 
-  const res = await axios.post(
-  'https://api.imgbb.com/1/upload',
-  form,
-  {
+  const res = await axios.post("https://api.imgbb.com/1/upload", form, {
     params: {
-      'key': process.env.IMG_BB
+      key: process.env.IMG_BB,
     },
     headers: {
-      ...form.getHeaders()
-    }
-  }
-  );
+      ...form.getHeaders(),
+    },
+  });
 
-  if(res.status != 200) { 
-  	response.status(500).json({message: "IMAGE API Error!"}).end();
+  if (res.status != 200) {
+    response.status(500).json({ message: "IMAGE API Error!" }).end();
   }
 
-  const query =
-    "UPDATE `user` SET imageUrl = ? WHERE id = ? LIMIT 1";
+  const query = "UPDATE `user` SET imageUrl = ? WHERE id = ? LIMIT 1";
 
   const [rows] = await connection.execute(query, [
     res.data.data.display_url,
-    body.userid
+    body.userid,
   ]);
 
   connection.end();
 
-  if(rows.affectedRows === 0) { 
-  	response.status(500).json({message: "Server Error!"}).end();
+  if (rows.affectedRows === 0) {
+    response.status(500).json({ message: "Server Error!" }).end();
   }
-  
-  response.status(200).json({imageUrl: res.data.data.display_url}).end();
+
+  response.status(200).json({ imageUrl: res.data.data.display_url }).end();
 };
 
-const changeName = async (request, response) => { 
+const changeName = async (request, response) => {
   let connection = mysql.createPool({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
@@ -133,24 +123,20 @@ const changeName = async (request, response) => {
 
   const body = request.body;
 
-  const query =
-    "UPDATE `user` SET name = ? WHERE id = ? LIMIT 1";
+  const query = "UPDATE `user` SET name = ? WHERE id = ? LIMIT 1";
 
-  const [rows] = await connection.execute(query, [
-    body.name,
-    body.userid
-  ]);
+  const [rows] = await connection.execute(query, [body.name, body.userid]);
 
   connection.end();
 
-  if(rows.affectedRows === 0) { 
-    return response.status(500).json({message: "Server Error!"}).end();
+  if (rows.affectedRows === 0) {
+    return response.status(500).json({ message: "Server Error!" }).end();
   }
-  
-  return response.status(200).end();
-}
 
-const changeBirthDate = async (request, response) => { 
+  return response.status(200).end();
+};
+
+const changeBirthDate = async (request, response) => {
   let connection = mysql.createPool({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
@@ -162,22 +148,21 @@ const changeBirthDate = async (request, response) => {
 
   const body = request.body;
 
-  const query =
-    "UPDATE `user` SET birthdate = ? WHERE id = ? LIMIT 1";
+  const query = "UPDATE `user` SET birthdate = ? WHERE id = ? LIMIT 1";
 
   const [rows] = await connection.execute(query, [
-    body.birthdate,
-    body.userid
+    body.birthdate.substr(0, 10),
+    body.userid,
   ]);
 
   connection.end();
 
-  if(rows.affectedRows === 0) { 
-    return response.status(500).json({message: "Server Error!"}).end();
+  if (rows.affectedRows === 0) {
+    return response.status(500).json({ message: "Server Error!" }).end();
   }
-  
+
   return response.status(200).end();
-}
+};
 
 module.exports = {
   changeSex,
