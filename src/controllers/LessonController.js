03607f44efa43756f235d6fe0aac6e676c5049c6
Guilherme Vianna <59426923+Guilherme-Vianna@users.noreply.github.com>
2023-10-17 21:02:46 -0300

    duplicate checkin bug fixed

--- src/controllers/LessonController.js
+++ src/controllers/LessonController.js
@@ -15,19 +15,21 @@ const createLesson = async (req, res) => {
 
   const body = req.body;
 
-  const [newlesson] = await connection.query("INSERT INTO `lesson` (title, professorName, startTime, endTime, maxVacancy, vacancy) VALUES (?,?,?,?,?,?)", [
-    body.title,
-    body.professorname,
-    body.starttime,
-    body.endtime,
-    body.maxvacancy,
-    0
-  ]);
+  const [newlesson] = await connection.query(
+    "INSERT INTO `lesson` (title, professorName, startTime, endTime, maxVacancy, vacancy) VALUES (?,?,?,?,?,?)",
+    [
+      body.title,
+      body.professorname,
+      body.starttime,
+      body.endtime,
+      body.maxvacancy,
+      0,
+    ]
+  );
 
   if (newlesson.affectedRows > 0) {
     res.status(201).end();
   }
-
 };
 
 const checkIn = async (req, res) => {
@@ -42,33 +44,42 @@ const checkIn = async (req, res) => {
 
   const body = req.body;
 
-  const [lesson] = await connection.query("SELECT * FROM `lesson` LIMIT 1");
-
+  const [lesson] = await connection.query(
+    "SELECT * FROM `lesson` WHERE id = ? LIMIT 1",
+    [body.lessonid]
+  );
+ 
   if (lesson[0].vacancy < lesson[0].maxVacancy) {
     const newVacancy = lesson[0].vacancy + 1;
 
-    const [checkinsituation] = await connection.query("SELECT * FROM `lessoncheckin` WHERE userid = ? AND lessonid = ? ", [
-      body.id,
-      body.lessonid
-    ])
+    const [checkinsituation] = await connection.query(
+      "SELECT * FROM `lessoncheckin` WHERE userid = ? AND lessonid = ? ",
+      [body.id, body.lessonid]
+    );
 
     if (checkinsituation.length > 0) {
-      return res.status(409).json({ message: "user is already checkedin" }).end();
+      return res
+        .status(409)
+        .json({ message: "user is already checkedin" })
+        .end();
     }
 
-    const [checkIn] = await connection.query("UPDATE `lesson` SET vacancy = ? WHERE id = ?", [
-      newVacancy,
-      body.lessonid,
-    ]);
+    const [checkIn] = await connection.query(
+      "UPDATE `lesson` SET vacancy = ? WHERE id = ? LIMIT 1",
+      [newVacancy, body.lessonid]
+    );
 
     if (checkIn.affectedRows > 0) {
-      const [checkIn] = await connection.query("INSERT INTO `lessoncheckin` (userid, lessonid) VALUES (?, ?)", [body.id, body.lessonid]);
+      await connection.query(
+        "INSERT INTO `lessoncheckin` (userid, lessonid) VALUES (?, ?)",
+        [body.id, body.lessonid]
+      );
       return res.status(200).end();
     }
   } else {
     return res.status(409).json({ message: "lesson is full" }).end();
   }
-}
+};
 
 const cancelCheckIn = async (req, res) => {
   let connection = mysql.createPool({
@@ -86,16 +97,19 @@ const cancelCheckIn = async (req, res) => {
 
   const newVacancy = lesson[0].vacancy - 1;
 
-  const [checkIn] = await connection.query("UPDATE `lesson` SET vacancy = ? WHERE id = ?", [
-    newVacancy,
-    body.lessonid,
-  ]);
+  const [checkIn] = await connection.query(
+    "UPDATE `lesson` SET vacancy = ? WHERE id = ?",
+    [newVacancy, body.lessonid]
+  );
 
   if (checkIn.affectedRows > 0) {
-    const [checkIn] = await connection.query("DELETE FROM `lessoncheckin` WHERE id = ? AND lessonid = ? LIMIT 1", [body.id, body.lessonid]);
+    const [checkIn] = await connection.query(
+      "DELETE FROM `lessoncheckin` WHERE id = ? AND lessonid = ? LIMIT 1",
+      [body.id, body.lessonid]
+    );
     return res.status(200).end();
   }
-}
+};
 
 const getLesson = async (req, res) => {
   let connection = mysql.createPool({
@@ -112,11 +126,11 @@ const getLesson = async (req, res) => {
   const [lesson] = await connection.query("SELECT * FROM `lesson`");
 
   return res.status(200).json(lesson);
-}
+};
 
 module.exports = {
   createLesson,
   checkIn,
   cancelCheckIn,
-  getLesson
-}
+  getLesson,
+};
