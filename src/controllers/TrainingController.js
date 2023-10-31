bac329168c6982feb0a92f2c669a937be742627c
Guilherme Vianna <mataveli91@gmail.com>
2023-10-31 11:22:01 -0700

    training controller refactored

--- src/controllers/TrainingController.js
+++ src/controllers/TrainingController.js
@@ -1,21 +1,11 @@
-const mysql = require("mysql2/promise");
-require("dotenv").config();
-const jwt = require("jsonwebtoken");
+const connection = require("../connection")
 
 const getCategories = async (req, res) => {
-  let connection = mysql.createPool({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
-    database: process.env.DATABASE_NAME,
-    connectionLimit: 1,
-  });
-
   const [categories] = await connection.query(
     "SELECT category FROM `training` WHERE name IS NULL"
   );
-  res.send(categories);
+
+  res.status(200).json(categories);
 };
 module.exports = {
   getCategories,
