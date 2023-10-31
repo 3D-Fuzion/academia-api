a59a7a2cf786b0046ea4bfc340d52ff8384536e5
Guilherme Vianna <mataveli91@gmail.com>
2023-10-31 11:02:54 -0700

    return removed

--- src/controllers/middlewares/ProfileMiddleware.js
+++ src/controllers/middlewares/ProfileMiddleware.js
@@ -1,183 +1,138 @@
-const mysql = require("mysql2/promise");
+const connection = require("../../connection")
 
-const validadeSetImageBody = async (req, res, next) => { 
-  let connection = mysql.createPool({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
-    database: process.env.DATABASE_NAME,
-    connectionLimit: 1,
-  });
+const validadeSetImageBody = async (req, res, next) => {
+  const { body } = req;
 
-  const body = req.body;
-
-  if(!body.userid) { 
-    return res.status(400).json({message: "USERID is required!"});
+  if (!body.userid) {
+    res.status(400).json({ message: "USERID is required!" });
   }
 
-  if(!body.image) { 
-    return res.status(400).json({message: "EFFECT_PHRASE is required!"});
+  if (!body.image) {
+    res.status(400).json({ message: "EFFECT_PHRASE is required!" });
   }
 
   const query = "SELECT * from `user` WHERE id = ? LIMIT 1"
 
-  const [rows] = await connection.execute(query,[ 
+  const [rows] = await connection.execute(query, [
     body.userid
   ]);
 
-  if(rows.length < 1) { 
-    return res.status(404).json({message: "User not found"});
+  if (rows.length < 1) {
+    res.status(404).json({ message: "User not found" });
   }
 
-  connection.end(); 
-  next(); 
+  connection.end();
+  next();
 }
 
-const validadeSexBody = async (req, res, next) => { 
- let connection = mysql.createPool({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
-    database: process.env.DATABASE_NAME,
-    connectionLimit: 1,
-  });
+const validadeSexBody = async (req, res, next) => {
+  const { body } = req;
 
-  const body = req.body;
-
-  if(!body.userid) { 
-    return res.status(400).json({message: "USERID is required!"});
+  if (!body.userid) {
+    res.status(400).json({ message: "USERID is required!" });
   }
 
-  if(!body.sex) { 
-    return res.status(400).json({message: "SEX is required!"});
+  if (!body.sex) {
+    res.status(400).json({ message: "SEX is required!" });
   }
 
   const query = "SELECT * from `user` WHERE id = ? LIMIT 1"
 
-  const [rows] = await connection.execute(query,[ 
+  const [rows] = await connection.execute(query, [
     body.userid
   ]);
 
-  if(rows.length < 1) { 
-    return res.status(404).json({message: "User not found"});
+  if (rows.length < 1) {
+    res.status(404).json({ message: "User not found" });
   }
 
-  connection.end(); 
-  next(); 
+  connection.end();
+  next();
 }
 
-const validadeEffectPhraseBody = async (req, res, next) => { 
- let connection = mysql.createPool({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
-    database: process.env.DATABASE_NAME,
-    connectionLimit: 1,
-  });
+const validadeEffectPhraseBody = async (req, res, next) => {
+  const { body } = req;
 
-  const body = req.body;
-
-  if(!body.userid) { 
-    return res.status(400).json({message: "USERID is required!"});
+  if (!body.userid) {
+    res.status(400).json({ message: "USERID is required!" });
   }
 
-  if(!body.effectphrase) { 
-    return res.status(400).json({message: "EFFECTPHRASE is required!"});
+  if (!body.effectphrase) {
+    res.status(400).json({ message: "EFFECTPHRASE is required!" });
   }
 
   const query = "SELECT * from `user` WHERE id = ? LIMIT 1"
 
-  const [rows] = await connection.execute(query,[ 
+  const [rows] = await connection.execute(query, [
     body.userid
   ]);
 
-  if(rows.length < 1) { 
-    return res.status(404).json({message: "User not found"});
+  if (rows.length < 1) {
+    res.status(404).json({ message: "User not found" });
   }
 
-  connection.end(); 
-  next(); 
+  next();
 }
 
-const validadeNameBody = async (req, res, next) => { 
-  let connection = mysql.createPool({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
-    database: process.env.DATABASE_NAME,
-    connectionLimit: 1,
-  });
+const validadeNameBody = async (req, res, next) => {
+  const { body } = req;
 
-  const body = req.body;
-  const regex = /[0-9]/; 
+  const regex = /[0-9]/;
 
-  if(!body.userid) { 
-    return res.status(400).json({message: "USERID is required!"});
-  } 
+  if (!body.userid) {
+    res.status(400).json({ message: "USERID is required!" });
+  }
 
-  if(!body.name) { 
-    return res.status(400).json({message: "NAME is required!"});
-  } else if (regex.test(body.name)) { 
-    return res.status(400).json({message: "NAME must be only letters!"});
+  if (!body.name) {
+    res.status(400).json({ message: "NAME is required!" });
+  } else if (regex.test(body.name)) {
+    res.status(400).json({ message: "NAME must be only letters!" });
   }
 
   const query = "SELECT * from `user` WHERE id = ? LIMIT 1"
 
-  const [rows] = await connection.execute(query,[ 
+  const [rows] = await connection.execute(query, [
     body.userid
   ]);
 
-  if(rows.length < 1) { 
-    return res.status(404).json({message: "User not found"});
+  if (rows.length < 1) {
+    res.status(404).json({ message: "User not found" });
   }
 
-  next(); 
+  next();
 }
 
-const validadeBirthDateBody = async (req, res, next) => { 
-  let connection = mysql.createPool({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
-    database: process.env.DATABASE_NAME,
-    connectionLimit: 1,
-  });
-
+const validadeBirthDateBody = async (req, res, next) => {
   const body = req.body;
-  const regex = new RegExp("[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]"); 
+  const regex = new RegExp("[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]");
 
-  if(!body.userid) { 
-    return res.status(400).json({message: "USERID is required!"});
-  } 
+  if (!body.userid) {
+    res.status(400).json({ message: "USERID is required!" });
+  }
 
-  if(!body.birthdate) { 
-    return res.status(400).json({message: "BIRTH_DATE is required!"});
-  } else if (!regex.test(body.birthdate)) { 
-    return res.status(400).json({message: "BIRTH_DATE format is incorrect!"});
+  if (!body.birthdate) {
+    res.status(400).json({ message: "BIRTH_DATE is required!" });
+  } else if (!regex.test(body.birthdate)) {
+    res.status(400).json({ message: "BIRTH_DATE format is incorrect!" });
   }
 
   const query = "SELECT * from `user` WHERE id = ? LIMIT 1"
 
-  const [rows] = await connection.execute(query,[ 
+  const [rows] = await connection.execute(query, [
     body.userid
   ]);
 
-  if(rows.length < 1) { 
-    return res.status(404).json({message: "User not found"});
+  if (rows.length < 1) {
+    res.status(404).json({ message: "User not found" });
   }
 
-  next(); 
+  next();
 }
 
 module.exports = {
-    validadeSetImageBody,
-    validadeSexBody,
-    validadeEffectPhraseBody,
-    validadeNameBody,
-    validadeBirthDateBody,
+  validadeSetImageBody,
+  validadeSexBody,
+  validadeEffectPhraseBody,
+  validadeNameBody,
+  validadeBirthDateBody,
 }
