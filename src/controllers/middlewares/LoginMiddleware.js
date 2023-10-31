207fb0c6ad2eca5d35e23d0407f282d13a09ee18
Guilherme Vianna <mataveli91@gmail.com>
2023-10-31 10:59:19 -0700

    return removed

--- src/controllers/middlewares/LoginMiddleware.js
+++ src/controllers/middlewares/LoginMiddleware.js
@@ -6,47 +6,47 @@ const validadeCreateUserBody = async (req, res, next) => {
   const { body } = req;
 
   if (!body.email) {
-    return res.status(400).json({ message: "EMAIL is required" });
+    res.status(400).json({ message: "EMAIL is required" });
   }
 
   if (!body.cpf) {
-    return res.status(400).json({ message: "CPF is required" });
+    res.status(400).json({ message: "CPF is required" });
   }
 
   if (!body.password) {
-    return res.status(400).json({ message: "PASSWORD is required" });
+    res.status(400).json({ message: "PASSWORD is required" });
   }
 
   if (!body.name) {
-    return res.status(400).json({ message: "NAME is required" });
+    res.status(400).json({ message: "NAME is required" });
   }
 
   if (!body.academycode) {
-    return res.status(400).json({ message: "ACADEMY_CODE is required" });
+    res.status(400).json({ message: "ACADEMY_CODE is required" });
   }
 
   if (!body.birthdate) {
-    return res.status(400).json({ message: "BIRTHDATE is required" });
+    res.status(400).json({ message: "BIRTHDATE is required" });
   }
 
   if (isNaN(body.cpf)) {
-    return res.status(400).json({ message: "CPF must be only numbers" });
+    res.status(400).json({ message: "CPF must be only numbers" });
   }
 
   if (body.cpf.length != 11) {
-    return res
+    res
       .status(400)
       .json({ errorCode: "2", message: "CPF format is incorrect" });
   }
 
   if (body.academycode.length != 8) {
-    return res
+    res
       .status(400)
       .json({ message: "ACADEMY_CODE format is incorrect" });
   }
 
   if (body.password.length < 8) {
-    return res.status(400).json({
+    res.status(400).json({
       errorCode: "3",
       message: "PASSWORD lenght is less than 8 characters",
     });
@@ -58,7 +58,7 @@ const validadeCreateUserBody = async (req, res, next) => {
   );
 
   if (rows.length > 0) {
-    return res
+    res
       .status(400)
       .json({ errorCode: "1", message: "EMAIL is already registered" })
       .end();
@@ -70,31 +70,32 @@ const validadeCreateUserBody = async (req, res, next) => {
   );
 
   if (academys.length < 1) {
-    return res
+    res
       .status(400)
       .json({ errorCode: "4", message: "Academy is not found" })
       .end();
   }
-  connection.end();
+
   next();
 };
+
 const validadeCredentialsBody = async (req, res, next) => {
   const { email, password } = req.body;
 
   if (email === undefined) {
-    return res.status(400).json({ message: "EMAIL is required" });
+    res.status(400).json({ message: "EMAIL is required" });
   }
 
   if (email === "") {
-    return res.status(400).json({ message: "EMAIL cannot by empty" });
+    res.status(400).json({ message: "EMAIL cannot by empty" });
   }
 
   if (password === undefined) {
-    return res.status(400).json({ message: "PASSWORD is required" });
+    res.status(400).json({ message: "PASSWORD is required" });
   }
 
   if (password === "") {
-    return res.status(400).json({ message: "PASSWORD cannot by empty" });
+    res.status(400).json({ message: "PASSWORD cannot by empty" });
   }
 
   const [executeResult] = await connection.execute(
@@ -103,18 +104,18 @@ const validadeCredentialsBody = async (req, res, next) => {
   );
 
   if (executeResult[0] === undefined) {
-    return res
+    res
       .status(404)
       .json({ message: "No entry with this email is detected" });
   }
   if (executeResult[0].registerStatus === "waiting") {
-    return res
+    res
       .status(403)
       .json({ errorCode: "5", message: "User is not accepted" });
   }
 
   if (executeResult[0].password != password) {
-    return res.status(400).json({ message: "Password is incorrect" });
+    res.status(400).json({ message: "Password is incorrect" });
   }
 
   res.locals.id = executeResult[0].id;
@@ -126,18 +127,10 @@ const validadeCredentialsBody = async (req, res, next) => {
 };
 
 const validadeChangePwdBody = async (req, res, next) => {
-  let connection = mysql.createPool({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
-    database: process.env.DATABASE_NAME,
-    connectionLimit: 1,
-  });
+  const { body } = req;
 
-  const body = req.body;
   if (body.newpassword.length < 8) {
-    return res.status(400).json({
+    res.status(400).json({
       errorCode: "3",
       message: "PASSWORD lenght is less than 8 characters",
     });
@@ -147,13 +140,12 @@ const validadeChangePwdBody = async (req, res, next) => {
     [body.email]
   );
   if (rows[0] === undefined) {
-    return res
+    res
       .status(404)
       .json({ message: "User with this email is not founded" });
   }
-  console.log(rows[0]);
   if (rows[0].cpf != body.cpf) {
-    return res
+    res
       .status(400)
       .json({ errorCode: "6", message: "Cpf is incorrect" });
   }
@@ -163,14 +155,15 @@ const validadeChangePwdBody = async (req, res, next) => {
 const verifyToken = async (req, res, next) => {
   const token = req.headers["auth-token"];
   if (token === undefined) {
-    return res.status(401).json({ message: "Please send token in auth-token" });
-  }
+    res.status(401).json({ message: "Please send token in auth-token" });
+  } else  { 
   jwt.verify(token, process.env.JWT_TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      return res.status(401).end();
+      res.status(401).end();
     }
     next();
   });
+  }
 };
 module.exports = {
   verifyToken,
