18523341b05abd2a758dfcbd2de8883117c662df
Guilherme Vianna <59426923+Guilherme-Vianna@users.noreply.github.com>
2023-11-01 20:25:03 -0300

    fix blank env

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const mysql = require("mysql2/promise");
-
+require('dotenv').config();
 const connection = mysql.createPool({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
@@ -7,3 +7,5 @@ const connection = mysql.createPool({
   port: process.env.DATABASE_PORT,
   database: process.env.DATABASE_NAME,
 });
+
+module.exports = connection;
