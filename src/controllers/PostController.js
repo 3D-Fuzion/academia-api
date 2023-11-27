40c2e42c67cc50ef32bd7bb40cca4d7efeee5a5a
Guilherme Vianna <59426923+Guilherme-Vianna@users.noreply.github.com>
2023-11-27 20:51:37 -0300

    todo added

---
+++ src/controllers/PostController.js
@@ -0,0 +1,10 @@
+const connection = require("../database")
+
+const createPublication = async (request, response) => {
+  //TODO CREATE PUBLICATION 
+};
+
+
+module.exports = {
+  createPublication,
+};
