4f3959604a5268d35f1717d523dad7017574eb52
Guilherme Vianna <59426923+Guilherme-Vianna@users.noreply.github.com>
2023-12-02 21:29:16 -0300

    title field added to query command

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -4,8 +4,8 @@ const createPublication = async (request, response) => {
   const { body } = request;
 
   await connection.query(
-    "INSERT INTO `post` (userid, image, likes) VALUES (?, ?, ?) ",
-    [body.userid, body.image, 0]
+    "INSERT INTO `post` (userid, image, likes, title) VALUES (?, ?, ?, ?) ",
+    [body.userid, body.image, 0, body.title]
   );
   return response.status(201).end() 
 };
