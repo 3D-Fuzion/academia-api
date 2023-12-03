ececb625d9d45721921d80cb55f839cfa2cdc713
Guilherme Vianna <59426923+Guilherme-Vianna@users.noreply.github.com>
2023-12-02 21:29:02 -0300

    title verification and return added

--- src/controllers/middlewares/PostMiddleware.js
+++ src/controllers/middlewares/PostMiddleware.js
@@ -1,12 +1,16 @@
 const validadeMakePost = async (req, res, next) => {
   const { body } = req;
 
-  if (!body.userid) {
-    res.status(400).json({ message: "USERID is required!" });
+  if (!body.userid || body.userid === " ") {
+    return res.status(400).json({ message: "USERID is required!" });
   }
 
   if (!body.image) {
-    res.status(400).json({ message: "IMAGE is required!" });
+    return res.status(400).json({ message: "IMAGE is required!" });
+  }
+
+  if (!body.title) {
+    return res.status(400).json({ message: "TITLE is required!" });
   }
 
   next();
